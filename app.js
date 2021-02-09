19ce1080e943c4ede5be1552b1c90406d3133695
Maciej Machniewski <machniewski.maciej@gmail.com>
2021-02-09 12:45:33 +0100

    Zmiany 09.02

--- app.js
+++ app.js
@@ -27,6 +27,42 @@ for (let i = 0; i <width*width; i++) {
  }
 createBoard()
 
+// drag the candies
+let colorBeingDragged
+let colorBeingReplaced
+let idBeingDragged
+
+
+squares.forEach(square => square.addEventListener('dragstart', dragStart))
+// squares.forEach(square => square.addEventListener('dragend', dragEnd))
+// squares.forEach(square => square.addEventListener('dragover', dragOver))
+// squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+// squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+// squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+
+function dragStart() {
+    colorBeingDragged = this.style.backgroundColor
+    idBeingDragged = parseInt(this.id)
+
+    console.log('dragStart, color: ' +colorBeingDragged + ' id: ' + idBeingDragged)
+
+}
+function dragEnd() {
+    console.log('dragEnd')
+}
+function dragOver() {
+    console.log('dragOver')
+}
+function dragEnter() {
+    console.log('dragEnter')
+}
+function dragLeave() {
+    console.log('dragLeave')
+}
+function dragDrop() {
+    console.log('dragDrop')
+}
  
 
 
