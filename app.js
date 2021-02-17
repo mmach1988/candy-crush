54c3818f0d6339a99e633de492f993d2f4839bff
Maciej Machniewski <machniewski.maciej@gmail.com>
2021-02-17 12:24:27 +0100

    Przed pracą domową - sprawdź błąd

--- app.js
+++ app.js
@@ -31,39 +31,45 @@ createBoard()
 let colorBeingDragged
 let colorBeingReplaced
 let idBeingDragged
+let idBeingReplaced
 
 
 squares.forEach(square => square.addEventListener('dragstart', dragStart))
-// squares.forEach(square => square.addEventListener('dragend', dragEnd))
-// squares.forEach(square => square.addEventListener('dragover', dragOver))
-// squares.forEach(square => square.addEventListener('dragenter', dragEnter))
-// squares.forEach(square => square.addEventListener('dragleave', dragLeave))
-// squares.forEach(square => square.addEventListener('drop', dragDrop))
+squares.forEach(square => square.addEventListener('dragend', dragEnd))
+squares.forEach(square => square.addEventListener('dragover', dragOver))
+squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+squares.forEach(square => square.addEventListener('drop', dragDrop))
 
 
 function dragStart() {
     colorBeingDragged = this.style.backgroundColor
     idBeingDragged = parseInt(this.id)
-
     console.log('dragStart, color: ' +colorBeingDragged + ' id: ' + idBeingDragged)
 
 }
 function dragEnd() {
     console.log('dragEnd')
 }
-function dragOver() {
+function dragOver(e) {
+    e.preventDefault()
     console.log('dragOver')
 }
-function dragEnter() {
+function dragEnter(e) {
+    e.preventDefault()
     console.log('dragEnter')
 }
 function dragLeave() {
     console.log('dragLeave')
 }
 function dragDrop() {
-    console.log('dragDrop')
-}
+    colorBeingReplaced = this.style.backgroundColor
+    idBeingReplaced = parseInt(this.id)
+    console.log('dragReplaced, color: ' +colorBeingReplaced + ' id: ' + idBeingReplaced)
+    this.style.backgroundColor = colorBeingDragged
+    squares[0].style.backgroundColor = 'red'
  
-
+// 1). W 70 wierszu zmienić tak zeby zmieniał się kolor klocka, który złapaliśmy (dragStart) na czerwony.
+// 2). jw + kolor klocka, który złapaliśmy (dragStart)
 
 })
